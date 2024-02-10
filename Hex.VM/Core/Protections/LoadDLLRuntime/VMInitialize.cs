ec8c48585b145ae0dbd1dbb272b5284751f3b8d8
TheHellTower <thehelltower@tuta.io>
2024-02-10 12:47:13 -0500

    DeflateStream => QuickLZ

--- Hex.VM/Core/Protections/LoadDLLRuntime/VMInitialize.cs
+++ Hex.VM/Core/Protections/LoadDLLRuntime/VMInitialize.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.IO.Compression;
 using System.Reflection;
 
 namespace Hex.VM.Core.Protections.LoadDLLRuntime
@@ -13,27 +12,204 @@ namespace Hex.VM.Core.Protections.LoadDLLRuntime
 
         private static byte[] array = null;
 
-        private static MemoryStream input;
+        public const int QLZ_VERSION_MAJOR = 1;
+        public const int QLZ_VERSION_MINOR = 5;
+        public const int QLZ_VERSION_REVISION = 0;
+        public const int QLZ_STREAMING_BUFFER = 0;
+        public const int QLZ_MEMORY_SAFE = 0;
+        private const int HASH_VALUES = 4096;
+        private const int UNCONDITIONAL_MATCHLEN = 6;
+        private const int UNCOMPRESSED_END = 4;
+        private const int CWORD_LEN = 4;
 
-        private static MemoryStream output;
 
         public static void InitializeRuntime() => AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
 
-        private static byte[] DecompressData(byte[] data)
-        {
-            input = new MemoryStream(data);
-            output = new MemoryStream();
-            using (DeflateStream dstream = new DeflateStream(input, CompressionMode.Decompress))
-                dstream.CopyTo(output);
-
-            return output.ToArray();
-        }
         private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
-            manifestResourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Hex.VM.Runtime.THT");
+            manifestResourceStream = Assembly.GetCallingAssembly().GetManifestResourceStream("Hex.VM.Runtime.THT");
             array = new byte[manifestResourceStream.Length];
             manifestResourceStream.Read(array, 0, array.Length);
-            assembly = Assembly.Load(DecompressData(array));
+            byte[] source = array;
+            int level;
+            int size = 0;
+            if ((((source[0] & 2) == 2) ? 9 : 3) == 9)
+                size = source[5] | (source[6] << 8) | (source[7] << 16) | (source[8] << 24);
+            else
+                size = source[2];
+            int src = (((source[0] & 2) == 2) ? 9 : 3);
+            int dst = 0;
+            uint cword_val = 1;
+            byte[] destination = new byte[size];
+            int[] hashtable = new int[4096];
+            byte[] hash_counter = new byte[4096];
+            int last_matchstart = size - UNCONDITIONAL_MATCHLEN - UNCOMPRESSED_END - 1;
+            int last_hashed = -1;
+            int hash;
+            uint fetch = 0;
+
+            level = (source[0] >> 2) & 0x3;
+
+            if ((source[0] & 1) != 1)
+            {
+                byte[] d2 = new byte[size];
+                Array.Copy(source, (((source[0] & 2) == 2) ? 9 : 3), d2, 0, size);
+                source = d2;
+                goto Final;
+            }
+
+            for (; ; )
+            {
+                if (cword_val == 1)
+                {
+                    cword_val = (uint)(source[src] | (source[src + 1] << 8) | (source[src + 2] << 16) | (source[src + 3] << 24));
+                    src += 4;
+                    if (dst <= last_matchstart)
+                    {
+                        if (level == 1)
+                            fetch = (uint)(source[src] | (source[src + 1] << 8) | (source[src + 2] << 16));
+                        else
+                            fetch = (uint)(source[src] | (source[src + 1] << 8) | (source[src + 2] << 16) | (source[src + 3] << 24));
+                    }
+                }
+
+                if ((cword_val & 1) == 1)
+                {
+                    uint matchlen;
+                    uint offset2;
+
+                    cword_val = cword_val >> 1;
+
+                    if (level == 1)
+                    {
+                        hash = ((int)fetch >> 4) & 0xfff;
+                        offset2 = (uint)hashtable[hash];
+
+                        if ((fetch & 0xf) != 0)
+                        {
+                            matchlen = (fetch & 0xf) + 2;
+                            src += 2;
+                        }
+                        else
+                        {
+                            matchlen = source[src + 2];
+                            src += 3;
+                        }
+                    }
+                    else
+                    {
+                        uint offset;
+                        if ((fetch & 3) == 0)
+                        {
+                            offset = (fetch & 0xff) >> 2;
+                            matchlen = 3;
+                            src++;
+                        }
+                        else if ((fetch & 2) == 0)
+                        {
+                            offset = (fetch & 0xffff) >> 2;
+                            matchlen = 3;
+                            src += 2;
+                        }
+                        else if ((fetch & 1) == 0)
+                        {
+                            offset = (fetch & 0xffff) >> 6;
+                            matchlen = ((fetch >> 2) & 15) + 3;
+                            src += 2;
+                        }
+                        else if ((fetch & 127) != 3)
+                        {
+                            offset = (fetch >> 7) & 0x1ffff;
+                            matchlen = ((fetch >> 2) & 0x1f) + 2;
+                            src += 3;
+                        }
+                        else
+                        {
+                            offset = (fetch >> 15);
+                            matchlen = ((fetch >> 7) & 255) + 3;
+                            src += 4;
+                        }
+                        offset2 = (uint)(dst - offset);
+                    }
+
+                    destination[dst + 0] = destination[offset2 + 0];
+                    destination[dst + 1] = destination[offset2 + 1];
+                    destination[dst + 2] = destination[offset2 + 2];
+
+                    for (int i = 3; i < matchlen; i += 1)
+                    {
+                        destination[dst + i] = destination[offset2 + i];
+                    }
+
+                    dst += (int)matchlen;
+
+                    if (level == 1)
+                    {
+                        fetch = (uint)(destination[last_hashed + 1] | (destination[last_hashed + 2] << 8) | (destination[last_hashed + 3] << 16));
+                        while (last_hashed < dst - matchlen)
+                        {
+                            last_hashed++;
+                            hash = (int)(((fetch >> 12) ^ fetch) & (HASH_VALUES - 1));
+                            hashtable[hash] = last_hashed;
+                            hash_counter[hash] = 1;
+                            fetch = (uint)(fetch >> 8 & 0xffff | destination[last_hashed + 3] << 16);
+                        }
+                        fetch = (uint)(source[src] | (source[src + 1] << 8) | (source[src + 2] << 16));
+                    }
+                    else
+                    {
+                        fetch = (uint)(source[src] | (source[src + 1] << 8) | (source[src + 2] << 16) | (source[src + 3] << 24));
+                    }
+                    last_hashed = dst - 1;
+                }
+                else
+                {
+                    if (dst <= last_matchstart)
+                    {
+                        destination[dst] = source[src];
+                        dst += 1;
+                        src += 1;
+                        cword_val = cword_val >> 1;
+
+                        if (level == 1)
+                        {
+                            while (last_hashed < dst - 3)
+                            {
+                                last_hashed++;
+                                int fetch2 = destination[last_hashed] | (destination[last_hashed + 1] << 8) | (destination[last_hashed + 2] << 16);
+                                hash = ((fetch2 >> 12) ^ fetch2) & (HASH_VALUES - 1);
+                                hashtable[hash] = last_hashed;
+                                hash_counter[hash] = 1;
+                            }
+                            fetch = (uint)(fetch >> 8 & 0xffff | source[src + 2] << 16);
+                        }
+                        else
+                        {
+                            fetch = (uint)(fetch >> 8 & 0xffff | source[src + 2] << 16 | source[src + 3] << 24);
+                        }
+                    }
+                    else
+                    {
+                        while (dst <= size - 1)
+                        {
+                            if (cword_val == 1)
+                            {
+                                src += CWORD_LEN;
+                                cword_val = 0x80000000;
+                            }
+
+                            destination[dst] = source[src];
+                            dst++;
+                            src++;
+                            cword_val = cword_val >> 1;
+                        }
+                        source = destination;
+                        goto Final;
+                    }
+                }
+            }
+            Final:
+            assembly = Assembly.Load(source);
             return assembly;
         }
     }
